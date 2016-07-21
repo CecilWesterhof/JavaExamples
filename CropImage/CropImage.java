ac1e483d84308da82b2b6d9c6def49fff124c403
Cecil Westerhof <CLDWesterhof@gmail.com>
2016-07-21 14:11:10 +0200

    Initialisation of crop and load in there own functions. Display information about the image and the select area.

--- CropImage/CropImage.java
+++ CropImage/CropImage.java
@@ -23,6 +23,7 @@ import java.util.TimeZone;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
@@ -57,9 +58,11 @@ public class CropImage extends Panel {
     static private JFileChooser   filesave;
     static private BufferedImage  image;
     static private int            imageHeight = 432;
+    static private JLabel         imageInfo   = new JLabel();
     static private int            imageWidth  = 648;
     static private Color          rectColor   = new Color(0, 0, 0);
     static private int            resizeValue = 8;
+    static private JLabel         selectInfo  = new JLabel();
     static private Stroke         stroke      = new BasicStroke(2.0f);
     static private int            x           = 0;
     static private int            y           = 0;
@@ -73,9 +76,9 @@ public class CropImage extends Panel {
             System.out.println("Did not select a file");
             System.exit(1);
         }
-        File input = fileopen.getSelectedFile();
+        File input  = fileopen.getSelectedFile();
         currentName = input.getAbsolutePath();
-        image  = ImageIO.read(input);
+        image       = ImageIO.read(input);
         photoWidth  = image.getWidth();
         photoHeight = image.getHeight();
         if (!verifyDimensions(photoWidth, photoHeight)) {
@@ -86,6 +89,12 @@ public class CropImage extends Panel {
         imageWidth  = photoWidth  / resizeValue;
         imageHeight = photoHeight / resizeValue;
         setSize(imageWidth, imageHeight);
+        imageInfo.setText(String.format( "Image size: %dx%d, displayed as: %dx%d",
+                                         photoWidth, photoHeight,
+                                         imageWidth, imageHeight));
+        selectInfo.setText(String.format("Crop size: %dx%d, displayed as: %dx%d",
+                                         cropWidth * resizeValue, cropHeight * resizeValue,
+                                         cropWidth,               cropHeight));
         addMouseListener(new MouseListener() {
                 @Override
                 public void mouseReleased(MouseEvent e) {
@@ -105,6 +114,80 @@ public class CropImage extends Panel {
                     y = (int) e.getPoint().getY() - cropHeight / 2;
                     verifyXAndY();
                     repaint();
+                }
+            });
+    }
+
+    static private void initCrop(JButton crop, Panel panel) {
+        crop.setToolTipText("To save the cropped image");
+        crop.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    int ret = filesave.showDialog(panel, "Open file");
+
+                    if (ret == JFileChooser.APPROVE_OPTION) {
+                        BufferedImage dest;
+                        JFrame        dispFrame;
+                        int           dispHeight = cropHeight * resizeValue;
+                        DispImage     dispImage;
+                        int           dispWidth  = cropWidth  * resizeValue;
+                        File          output     = filesave.getSelectedFile();
+
+                        dest       = image.getSubimage(x * resizeValue, y * resizeValue,
+                                                       dispWidth,       dispHeight);
+                        dispFrame = new JFrame(output.getName());
+                        dispImage = new DispImage(dest, dispWidth, dispHeight);
+                        dispFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                        dispFrame.add(dispImage);
+                        dispFrame.setSize(dispWidth + 10, dispHeight + 40);
+                        dispFrame.setVisible(true);
+
+                        try {
+                            ImageIO.write(dest, "JPG", output);
+                        } catch (IOException e) {
+                            System.out.println("Could not write: " + output.getAbsolutePath());
+                        }
+                    }
+                }
+            });
+    }
+
+    static private void initLoad(JButton load, Panel panel) {
+        load.setToolTipText("To load a new image");
+        load.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    BufferedImage  newImage = null;
+                    String         newName;
+                    int ret = fileopen.showDialog(panel, "Open file");
+
+                    if (ret == JFileChooser.APPROVE_OPTION) {
+                        File input = fileopen.getSelectedFile();
+
+                        newName = input.getAbsolutePath();
+                        try {
+                            newImage = ImageIO.read(input);
+                        } catch (IOException e) {
+                            System.out.println("Could not read the input file: " + newName);
+                        }
+                        if (newImage != null) {
+                            int photoWidth  = newImage.getWidth();
+                            int photoHeight = newImage.getHeight();
+
+                            if (!verifyDimensions(photoWidth, photoHeight)) {
+                                System.out.println(String.format("Dimensions %s not correct",
+                                                                 newName));
+                            } else {
+                                imageWidth  = photoWidth  / resizeValue;
+                                imageHeight = photoHeight / resizeValue;
+                                imageInfo.setText(String.format( "Image size: %dx%d, displayed as: %dx%d",
+                                                                 photoWidth, photoHeight,
+                                                                 imageWidth, imageHeight));
+                                currentName = newName;
+                                image       = newImage;
+                                verifyXAndY();
+                                panel.repaint();
+                            }
+                        }
+                    }
                 }
             });
     }
@@ -160,78 +243,19 @@ public class CropImage extends Panel {
                                                   localCalendar.get(Calendar.YEAR),
                                                   localCalendar.get(Calendar.MONTH) + 1));
         panel    = new CropImage();
+        initCrop(crop, panel);
+        initLoad(load, panel);
         frame.setLayout(null);
-        crop.setToolTipText("To save the cropped image");
-        crop.setBounds(10, imageHeight + 10, 80, 30);
-        crop.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent event) {
-                    int ret = filesave.showDialog(panel, "Open file");
-
-                    if (ret == JFileChooser.APPROVE_OPTION) {
-                        BufferedImage dest;
-                        JFrame        dispFrame;
-                        int           dispHeight = cropHeight * resizeValue;
-                        DispImage     dispImage;
-                        int           dispWidth  = cropWidth  * resizeValue;
-                        File          output     = filesave.getSelectedFile();
-
-                        dest       = image.getSubimage(x * resizeValue, y * resizeValue,
-                                                       dispWidth,       dispHeight);
-                        dispFrame = new JFrame(output.getName());
-                        dispImage = new DispImage(dest, dispWidth, dispHeight);
-                        dispFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                        dispFrame.add(dispImage);
-                        dispFrame.setSize(dispWidth + 10, dispHeight + 40);
-                        dispFrame.setVisible(true);
-
-                        try {
-                            ImageIO.write(dest, "JPG", output);
-                        } catch (IOException e) {
-                            System.out.println("Could not write: " + output.getAbsolutePath());
-                        }
-                    }
-                }
-            });
-        load.setToolTipText("To load a new image");
-        load.setBounds(110, imageHeight + 10, 80, 30);
-        load.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent event) {
-                    BufferedImage  newImage = null;
-                    String         newName;
-                    int ret = fileopen.showDialog(panel, "Open file");
-
-                    if (ret == JFileChooser.APPROVE_OPTION) {
-                        File input = fileopen.getSelectedFile();
-
-                        newName = input.getAbsolutePath();
-                        try {
-                            newImage = ImageIO.read(input);
-                        } catch (IOException e) {
-                            System.out.println("Could not read the input file: " + newName);
-                        }
-                        if (newImage != null) {
-                            int photoWidth  = newImage.getWidth();
-                            int photoHeight = newImage.getHeight();
-
-                            if (!verifyDimensions(photoWidth, photoHeight)) {
-                                System.out.println(String.format("Dimensions %s not correct",
-                                                                 newName));
-                            } else {
-                                imageWidth  = photoWidth  / resizeValue;
-                                imageHeight = photoHeight / resizeValue;
-                                currentName = newName;
-                                image       = newImage;
-                                verifyXAndY();
-                                panel.repaint();
-                            }
-                        }
-                    }
-                }
-            });
+        crop.setBounds(      10, imageHeight + 10,  80, 30);
+        load.setBounds(     110, imageHeight + 10,  80, 30);
+        imageInfo.setBounds( 10, imageHeight + 50, 500, 30);
+        selectInfo.setBounds(10, imageHeight + 90, 500, 30);
         frame.getContentPane().add(panel);
         frame.getContentPane().add(crop);
         frame.getContentPane().add(load);
-        frame.setSize(imageWidth + 10, imageHeight + 80);
+        frame.getContentPane().add(imageInfo);
+        frame.getContentPane().add(selectInfo);
+        frame.setSize(imageWidth + 10, imageHeight + 160);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
